0972d9be59b76d88599ea05cfa8f8f31420d4ed0
Yafuo <44750263+Yafuo@users.noreply.github.com>
2019-11-07 18:49:33 +0700

    Add files via upload

--- public/js/clock.js
+++ public/js/clock.js
@@ -4,48 +4,45 @@ $(document).ready(function () {
     pack.forEach( (p, i) => {
         $('.package').append(`<tr class="pack pack${i}" data-ab="${i}"><td>${p}</td></tr>`);
     });
-    $('#booking').click( function (e)  {
-        console.log(parkingSlotsJS);
-        // $.ajax({
-        //     type: 'POST',
-        //     url: '/booking',
-        //
-        // });
-    });
+    // $('#booking').click( function (e)  {
+    //     console.log(parkingSlotsJS);
+    // });
     $(".package").delegate('tr','click', null, function (e) {
         $('.pack').removeClass('bg-green');
         var i = $(this).data()['ab'];
         $(`.pack${i}`).addClass('bg-green');
         $('#select-pack').removeClass('disabled');
     })
-    $('#paying').click(e => {
+    $('#booking').click(e => {
         var date = Date.now().toString();
-        var data = `partnerCode=MOMO&accessKey=F8BBA842ECF85&requestId=UIT${date}&amount=5000&orderId=UIT${date}&orderInfo=UIT team.&returnUrl=https://momo.vn&notifyUrl=https://momo.vn&extraData=abc@gmail.com`;
+        var d = {
+            partnerCode: 'MOMO',
+            accessKey: 'F8BBA842ECF85',
+            requestId: 'UIT'+ date,
+            amount: '5000',
+            orderId: 'UIT'+ date,
+            orderInfo: 'UIT team.',
+            returnUrl: 'https://141a182b.ngrok.io/',
+            notifyUrl: 'https://141a182b.ngrok.io/receive-notify',
+            requestType: 'captureMoMoWallet',
+            extraData: 'abc@gmail.com',
+        }
+        var data = `partnerCode=${d.partnerCode}&accessKey=${d.accessKey}&requestId=${d.requestId}&amount=${d.amount}&orderId=${d.orderId}&orderInfo=${d.orderInfo}&returnUrl=${d.returnUrl}&notifyUrl=${d.notifyUrl}&extraData=${d.extraData}`;
         var secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
         var signature = CryptoJS.HmacSHA256(data, secretKey);
-        console.log(signature.toString(CryptoJS.enc.Hex));
+        d.signature = signature.toString(CryptoJS.enc.Hex);
+        // console.log(d);
+        // console.log(signature.toString(CryptoJS.enc.Hex));
         $.ajax({
             type: 'POST',
             url: 'https://test-payment.momo.vn/gw_payment/transactionProcessor',
-            data: JSON.stringify({
-                partnerCode: 'MOMO',
-                accessKey: 'F8BBA842ECF85',
-                requestId: 'UIT'+ date,
-                amount: '5000',
-                orderId: 'UIT'+ date,
-                orderInfo: 'UIT team.',
-                returnUrl: 'https://momo.vn',
-                notifyUrl: 'https://momo.vn',
-                requestType: 'captureMoMoWallet',
-                signature: signature.toString(CryptoJS.enc.Hex),
-                extraData: 'abc@gmail.com',
-            }),
+            data: JSON.stringify(d),
             crossDomain: true,
             success: function (data) {
                 var prefix = 'https://test-payment.momo.vn/gw_payment/qrcode/image/receipt?key=';
                 var qrUrl = prefix + data.qrCodeUrl.slice(42);
-                console.log(data);
-                console.log(qrUrl);
+                // console.log(data);
+                // console.log(qrUrl);
                 $('#qr-code').attr('src', qrUrl);
             },
             headers: {
